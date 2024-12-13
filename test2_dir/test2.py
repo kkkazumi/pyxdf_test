a137e51fcee7e9d212dbd59ddfabec904d2788f2
Kazumi Kumagai <kazumi.kumagai@riken.jp>
2024-12-13 15:20:23 +0900

    updated to get graphs during speech task

--- test2_dir/test2.py
+++ test2_dir/test2.py
@@ -1,55 +1,68 @@
 import pyxdf
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 
 VITAL_SYS_CHANNEL=11
 VITAL_HR_CHANNEL=14
 SV=70
 
-def check_marker(org_marker):
+#graph drawing
+GRAPH_VER = 1
+GRAPH_HOR = 3
+GRAPH_SYS = 1
+GRAPH_CO = 2
+GRAPH_SVR = 3
+
+
+def check_marker(org_marker,filename):
     new_marker = np.zeros((1,2))
     count=0
-    for i in org_marker:
-        print("check marker:",i)
-        print("select: 0)keep, 1)delete, 2)change")
-        mode=int(input())
-        if(mode==0):
-            if(count==0):
-                new_marker[0]=i
-                count=1
-            else:
-                new_marker=np.vstack((new_marker,i))
-        elif(mode==2):
-            print("input new value")
-            new_val = int(input())
-            i[1]=new_val
-            if(count==0):
-                new_marker[0]=i
-                count=1
-            else:
-                new_marker=np.vstack((new_marker,i))
+    if not os.path.exists(filename):
+        for i in org_marker:
+            print("check marker:",i)
+            print("select: 0)keep, 1)delete, 2)change")
+            mode=int(input())
+            if(mode==0):
+                if(count==0):
+                    new_marker[0]=i
+                    count=1
+                else:
+                    new_marker=np.vstack((new_marker,i))
+            elif(mode==2):
+                print("input new value")
+                new_val = int(input())
+                i[1]=new_val
+                if(count==0):
+                    new_marker[0]=i
+                    count=1
+                else:
+                    new_marker=np.vstack((new_marker,i))
         print("new marker",new_marker)
+    else:
+        new_marker = np.loadtxt(filename,delimiter=",")
     return new_marker
+
 
 # Aの値が特定の範囲にある場合にBの値を抽出する関数
 def extract_values(A, B):
-    SP_BP = []
+    NH_BP = []
     RB_BP = []
     HM_BP = []
     for i in range(len(A) - 1):
         if A[i][1] == 1010 and A[i + 1][1] == 1500:
             start_time = A[i][0]
             end_time = A[i + 1][0]
-            SP_BP.extend(B[(B[:, 0] > start_time) & (B[:, 0] < end_time), 1])
-        elif A[i][1] == 200 and A[i + 1][1] == 250:
+            NH_BP.extend(B[(B[:, 0] > start_time) & (B[:, 0] < end_time), 1])
+        elif A[i][1] == 2010 and A[i + 1][1] == 2500:
             start_time = A[i][0]
             end_time = A[i + 1][0]
             RB_BP.extend(B[(B[:, 0] > start_time) & (B[:, 0] < end_time), 1])
-        elif A[i][1] == 300 and A[i + 1][1] == 350:
+        elif A[i][1] == 3010 and A[i + 1][1] == 3500:
             start_time = A[i][0]
             end_time = A[i + 1][0]
             HM_BP.extend(B[(B[:, 0] > start_time) & (B[:, 0] < end_time), 1])
-    return SP_BP, RB_BP, HM_BP
+    return NH_BP, RB_BP, HM_BP
 
 def attach_timestamp(org_array,timestamp_stream):
     new_array = np.zeros((org_array.shape[0],2))
@@ -91,17 +104,50 @@ for stream in data:
                 svr=attach_timestamp(_svr,stream['time_stamps'])
                 co=attach_timestamp(_co,stream['time_stamps'])
                 sys=attach_timestamp(_sys,stream['time_stamps'])
-                print("svr",svr)
         else:
-            plt.plot(stream['time_stamps'], y)
+            #plt.plot(stream['time_stamps'], y)
             marker=attach_timestamp(y[:,0],stream['time_stamps'])
             print("marker data",marker)
-            marker=check_marker(marker)
-            print("new marker data",marker)
+            filename = './'+dirname+'/new_marker.csv'
+            marker=check_marker(marker,filename)
+            np.savetxt(filename, marker, delimiter=',')
+
 
     else:
         raise RuntimeError('Unknown stream format')
 
-SP_BP, RB_BP, HM_BP = extract_values(marker, svr)
-print(len(SP_BP),len(RB_BP),len(HM_BP))
-plt.savefig("./"+dirname+"/save.png")
+plt.figure(figsize=(18,6))
+plt.subplot(GRAPH_VER,GRAPH_HOR,GRAPH_SYS)
+NH_SYS, RB_SYS, HM_SYS = extract_values(marker, sys)
+print(len(NH_SYS),len(RB_SYS),len(HM_SYS))
+#plt.savefig("./"+dirname+"/save.png")
+plt.plot(NH_SYS,label="nohint")
+plt.plot(RB_SYS,label="robot")
+plt.plot(HM_SYS,label="human")
+plt.xlabel("timestamp[sec]")
+plt.ylabel("value (sys)")
+plt.legend()
+
+plt.subplot(GRAPH_VER,GRAPH_HOR,GRAPH_CO)
+NH_CO, RB_CO, HM_CO = extract_values(marker, co)
+print(len(NH_CO),len(RB_CO),len(HM_CO))
+#plt.savefig("./"+dirname+"/save.png")
+plt.plot(NH_CO,label="nohint")
+plt.plot(RB_CO,label="robot")
+plt.plot(HM_CO,label="human")
+plt.xlabel("timestamp[sec]")
+plt.ylabel("value (co)")
+plt.legend()
+
+plt.subplot(GRAPH_VER,GRAPH_HOR,GRAPH_SVR)
+NH_SVR, RB_SVR, HM_SVR = extract_values(marker, svr)
+print(len(NH_SVR),len(RB_SVR),len(HM_SVR))
+#plt.savefig("./"+dirname+"/save.png")
+plt.plot(NH_SVR,label="nohint")
+plt.plot(RB_SVR,label="robot")
+plt.plot(HM_SVR,label="human")
+plt.xlabel("timestamp[sec]")
+plt.ylabel("value (svr)")
+plt.legend()
+
+plt.savefig("./"+dirname+"/"+dirname+"_taskdata.png")
