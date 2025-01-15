7268349dae2f1f43a6fb238d3cf782b3c52711fd
Kazumi Kumagai <kazumi.kumagai@riken.jp>
2025-01-15 11:27:10 +0900

    updated record data timestamp

--- test2_dir/test2.py
+++ test2_dir/test2.py
@@ -46,23 +46,32 @@ def check_marker(org_marker,filename):
 
 # Aの値が特定の範囲にある場合にBの値を抽出する関数
 def extract_values(A, B):
+
     NH_BP = []
     RB_BP = []
     HM_BP = []
+
+    tNH_BP = []
+    tRB_BP = []
+    tHM_BP = []
+
     for i in range(len(A) - 1):
         if A[i][1] == 1010 and A[i + 1][1] == 1500:
             start_time = A[i][0]
             end_time = A[i + 1][0]
             NH_BP.extend(B[(B[:, 0] > start_time) & (B[:, 0] < end_time), 1])
+            tNH_BP.extend(B[(B[:, 0] > start_time) & (B[:, 0] < end_time), 0]-start_time)
         elif A[i][1] == 2010 and A[i + 1][1] == 2500:
             start_time = A[i][0]
             end_time = A[i + 1][0]
             RB_BP.extend(B[(B[:, 0] > start_time) & (B[:, 0] < end_time), 1])
+            tRB_BP.extend(B[(B[:, 0] > start_time) & (B[:, 0] < end_time), 0]-start_time)
         elif A[i][1] == 3010 and A[i + 1][1] == 3500:
             start_time = A[i][0]
             end_time = A[i + 1][0]
             HM_BP.extend(B[(B[:, 0] > start_time) & (B[:, 0] < end_time), 1])
-    return NH_BP, RB_BP, HM_BP
+            tHM_BP.extend(B[(B[:, 0] > start_time) & (B[:, 0] < end_time), 0]-start_time)
+    return NH_BP, RB_BP, HM_BP, tNH_BP, tRB_BP, tHM_BP
 
 def attach_timestamp(org_array,timestamp_stream):
     new_array = np.zeros((org_array.shape[0],2))
@@ -70,12 +79,15 @@ def attach_timestamp(org_array,timestamp_stream):
     new_array[:,0]= timestamp_stream
     return new_array
                 
+#main
+
 print("input the dirname")
 dirname = input()
 filename = "./"+dirname+"/"+dirname+"data.xdf"
 
 data, header = pyxdf.load_xdf(filename)
 print("header",header)
+
 
 for stream in data:
     y = stream['time_series']
@@ -116,38 +128,45 @@ for stream in data:
     else:
         raise RuntimeError('Unknown stream format')
 
+    
 plt.figure(figsize=(18,6))
 plt.subplot(GRAPH_VER,GRAPH_HOR,GRAPH_SYS)
-NH_SYS, RB_SYS, HM_SYS = extract_values(marker, sys)
-print(len(NH_SYS),len(RB_SYS),len(HM_SYS))
+NH_SYS, RB_SYS, HM_SYS ,tNH_SYS, tRB_SYS, tHM_SYS = extract_values(marker, sys)
+print("nh",len(NH_SYS),"rb",len(RB_SYS),"hm",len(HM_SYS))
 #plt.savefig("./"+dirname+"/save.png")
-plt.plot(NH_SYS,label="nohint")
-plt.plot(RB_SYS,label="robot")
-plt.plot(HM_SYS,label="human")
+plt.plot(tNH_SYS,NH_SYS,label="nohint")
+plt.plot(tRB_SYS,RB_SYS,label="robot")
+plt.plot(tHM_SYS,HM_SYS,label="human")
 plt.xlabel("timestamp[sec]")
 plt.ylabel("value (sys)")
+plt.axvline(x=76,color="black",linestyle="--")
+plt.axvline(x=136,color="black",linestyle="--")
 plt.legend()
 
 plt.subplot(GRAPH_VER,GRAPH_HOR,GRAPH_CO)
-NH_CO, RB_CO, HM_CO = extract_values(marker, co)
+NH_CO, RB_CO, HM_CO ,tNH_CO, tRB_CO, tHM_CO = extract_values(marker, co)
 print(len(NH_CO),len(RB_CO),len(HM_CO))
 #plt.savefig("./"+dirname+"/save.png")
-plt.plot(NH_CO,label="nohint")
-plt.plot(RB_CO,label="robot")
-plt.plot(HM_CO,label="human")
+plt.plot(tNH_CO,NH_CO,label="nohint")
+plt.plot(tRB_CO,RB_CO,label="robot")
+plt.plot(tHM_CO,HM_CO,label="human")
 plt.xlabel("timestamp[sec]")
 plt.ylabel("value (co)")
+plt.axvline(x=76,color="black",linestyle="--")
+plt.axvline(x=136,color="black",linestyle="--")
 plt.legend()
 
 plt.subplot(GRAPH_VER,GRAPH_HOR,GRAPH_SVR)
-NH_SVR, RB_SVR, HM_SVR = extract_values(marker, svr)
+NH_SVR, RB_SVR, HM_SVR, tNH_SVR, tRB_SVR, tHM_SVR = extract_values(marker, svr)
 print(len(NH_SVR),len(RB_SVR),len(HM_SVR))
 #plt.savefig("./"+dirname+"/save.png")
-plt.plot(NH_SVR,label="nohint")
-plt.plot(RB_SVR,label="robot")
-plt.plot(HM_SVR,label="human")
+plt.plot(tNH_SVR,NH_SVR,label="nohint")
+plt.plot(tRB_SVR,RB_SVR,label="robot")
+plt.plot(tHM_SVR,HM_SVR,label="human")
 plt.xlabel("timestamp[sec]")
 plt.ylabel("value (svr)")
+plt.axvline(x=76,color="black",linestyle="--")
+plt.axvline(x=136,color="black",linestyle="--")
 plt.legend()
 
 plt.savefig("./"+dirname+"/"+dirname+"_taskdata.png")
